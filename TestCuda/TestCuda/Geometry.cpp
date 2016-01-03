b36dc2efabc92c834be4d68f28ac70aba088662f
Botezatu Mihai Catalin <botezatu.mihaicatalin@gmail.com>
2016-01-04 00:19:00 +0200

    Small fixes

--- TestCuda/TestCuda/Geometry.cpp
+++ TestCuda/TestCuda/Geometry.cpp
@@ -33,16 +33,11 @@ Circle findCircumcircle(const Point & p1, const Point & p2, const Point & p3)
 	
 	if (p12_dx == 0 && p12_dy != 0 && p23_dy != 0 && p23_dx != 0)
 	{
-		double slope2 = (p2.y - p3.y) / (p2.x - p3.x);
+		double slope2 = p23_dy / p23_dx;
 		double bisect_slope2 = -1 / slope2;
 
 		double y = (p1.y + p2.y) / 2;
 		double x = (y - mid2.y + bisect_slope2 * mid2.x) / bisect_slope2;
-
-		// (y - mid2.y) = bisect_slope2 * (x - mid2.x)
-		// y = (p1.y + p2.y) / 2;
-		// ((p1.y + p2.y) / 2 - mid2.y) = bisect_slope2 * (x - mid2.x);
-		// x = ((p1.y + p2.y) / 2 - mid2.y + bisect_slope2 * mid2.x) / bisect_slope2;
 
 		Circle c;
 		c.center.x = x;
@@ -54,7 +49,7 @@ Circle findCircumcircle(const Point & p1, const Point & p2, const Point & p3)
 
 	if (p12_dy == 0 && p12_dx != 0 && p23_dx != 0 && p23_dy != 0)
 	{
-		double slope2 = (p2.y - p3.y) / (p2.x - p3.x);
+		double slope2 = p23_dy / p23_dx;
 		double bisect_slope2 = -1 / slope2;
 
 		double x = (p1.x + p2.x) / 2;
@@ -66,23 +61,15 @@ Circle findCircumcircle(const Point & p1, const Point & p2, const Point & p3)
 		c.radius = dist(p1, c.center);
 
 		return c;
-		
-		// x = (p1.x + p2.x) / 2;
-		// (y - mid2.y) = bisect_slope2 * (x - mid2.x)
-		// y = bisect_slope2 * ((p1.x + p2.x) / 2 - mid2.x) + mid2.y;
 	}
 
 	if (p12_dx != 0 && p12_dy != 0 && p23_dy == 0 && p23_dx != 0)
 	{
-		double slope1 = (p1.y - p2.y) / (p1.x - p2.x);
+		double slope1 = p12_dy / p12_dx;
 		double bisect_slope1 = -1 / slope1;
 
 		double x = (p2.x + p3.x) / 2;
 		double y = bisect_slope1 * (x - mid1.x) + mid1.y;
-
-		// x = (p2.x + p3.x) / 2
-		// (y - mid1.y) = bisect_slope1 * (x - mid1.x)
-		// y = bisect_slope1 * (x - mid1.x) + mid1.y
 
 		Circle c;
 		c.center.x = x;
@@ -94,15 +81,11 @@ Circle findCircumcircle(const Point & p1, const Point & p2, const Point & p3)
 
 	if (p12_dx != 0 && p12_dy != 0 && p23_dy != 0 && p23_dx == 0)
 	{
-		double slope1 = (p1.y - p2.y) / (p1.x - p2.x);
+		double slope1 = p12_dy / p12_dx;
 		double bisect_slope1 = -1 / slope1;
 
 		double y = (p2.y + p3.y) / 2;
 		double x = (y - mid1.y + mid1.x * bisect_slope1) / bisect_slope1;
-
-		// (y - mid1.y) = bisect_slope1 * (x - mid1.x)
-		// y = (p2.y + p3.y) / 2;
-		// x = (y - mid1.y + mid1.x * bisect_slope1) / bisect_slope1;
 
 		Circle c;
 		c.center.x = x;
@@ -113,30 +96,16 @@ Circle findCircumcircle(const Point & p1, const Point & p2, const Point & p3)
 	}
 	
 
-	double slope1 = (p1.y - p2.y) / (p1.x - p2.x);
-	double slope2 = (p2.y - p3.y) / (p2.x - p3.x);
+	double slope1 = p12_dy / p12_dx;
+	double slope2 = p23_dy / p23_dx;
 
 	if (slope1 == slope2)
 	{
-
-
-		/*Circle c;
-		c.center.x = x;
-		c.center.y = y;
-		c.radius = dist(p1, c.center);
-
-		return c;*/
+		// FIX ME: Treat the case when all points are colinear!!
 	}
 
 	double bisect_slope1 = -1 / slope1;
 	double bisect_slope2 = -1 / slope2;
-	
-	// (y - mid1.y) = bisect_slope1 * (x - mid1.x)
-	// (y - mid2.y) = bisect_slope2 * (x - mid2.x)
-	// y = bisect_slope2 * (x - mid2.x) + mid2.y
-	// (bisect_slope2 * (x - mid2.x) + mid2.y - mid1.y) = bisect_slope1 * (x - mid1.x)
-	// x * (bisect_slope2 - bisect_slope1) = bisect_slope2 * mid2.x - bisect_slope1 * mid1.x + mid1.y - mid2.y
-	// x = (bisect_slope2 * mid2.x - bisect_slope1 * mid1.x + mid1.y - mid2.y) / (bisect_slope2 - bisect_slope1)
 
 	double x = (bisect_slope2 * mid2.x - bisect_slope1 * mid1.x + mid1.y - mid2.y) / (bisect_slope2 - bisect_slope1);
 	double y = (bisect_slope2 * (x - mid2.x) + mid2.y);
